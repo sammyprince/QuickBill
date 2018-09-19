e0e9d08f46221ca81a02d017a14b1eadc707c8d3
sammyprince <43393283+sammyprince@users.noreply.github.com>
2018-09-19 20:05:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const path = require("path");
+const path = require("https://sammyprince.github.io/QuickBill");
 const express = require("express");
 
 const app = express();
 
-app.use('/dist', express.static(__dirname + '/dist'));
-app.use('/assets', express.static(__dirname + '/assets'));
+app.use('https://sammyprince.github.io/QuickBill/dist', express.static(__dirname + '/dist'));
+app.use('https://sammyprince.github.io/QuickBill/assets', express.static(__dirname + '/assets'));
 
 app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'https://sammyprince.github.io/QuickBill/index.html'));
 });
 
 app.listen(8000, function () {
