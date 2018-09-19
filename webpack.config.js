c4da751fbc4009d59639e71fd041351cd56af139
sammyprince <43393283+sammyprince@users.noreply.github.com>
2018-09-19 20:00:12 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,37 +10,3 @@ let BASE_URL = {
 module.exports = {
     entry: {
         index: 'https://sammyprince.github.io/QuickBill/app/index.js'
-    },
-    output: {
-        path: path.join(__dirname, "./dist"),
-        publicPath: "/dist/",
-        filename: "[name].bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    }
-}
-
-if (env === 'production') {
-    config.plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                pure_getters: true,
-                unsafe: true,
-                unsafe_comps: true,
-                warnings: false,
-            },
-            output: {
-                comments: false,
-            },
-            sourceMap: false,
-        })
-    );
-}
-
