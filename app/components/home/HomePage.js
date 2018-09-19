96c3ff9f4e8694d14950632f50e37794882237f2
sammyprince <43393283+sammyprince@users.noreply.github.com>
2018-09-19 19:20:11 +0100

    Update HomePage.js

--- app/components/home/HomePage.js
+++ app/components/home/HomePage.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 export default class HomePage extends Component {
     render() {
@@ -7,7 +6,7 @@ export default class HomePage extends Component {
             <div className="home">
                 <div className="home__banner home__banner--bg">
                     <div className="overlay"></div>
-                    <h2>Frees i and Simple <span>invoicing</span> for freelancers and businesses.</h2>
+                    <h2>Frees i and Simple <span>invoicings</span> for freelancers and businesses.</h2>
                     <Link to="/QuickBill/dashboard" className="solid-btn">Create Invoice</Link>
                     <h4>No signup required</h4>
                 </div>
@@ -15,19 +14,19 @@ export default class HomePage extends Component {
                     <h4>Star this project on Github</h4>
                     <div className="github-buttons">
                         <a className="github-button"
-                            href="https://github.com/PunitGr/QuickBill"
+                            href="https://sammyprince.github.io/QuickBill/"
                             data-icon="octicon-star"
                             data-size="large"
                             data-show-count="true"
-                            aria-label="Star PunitGr/QuickBill on GitHub"
+                            aria-label="Star sammyprince/QuickBill/ on GitHub"
                             alt="github-star">
                                 Star
                         </a>
                         <a className="github-button"
-                            href="https://github.com/PunitGr/QuickBill/fork"
+                            href="https://sammyprince.github.io/QuickBill/fork"
                             data-icon="octicon-repo-forked" data-size="large"
                             data-show-count="true"
-                            aria-label="Fork PunitGr/QuickBill on GitHub"
+                            aria-label="Fork sammyprince/QuickBill on GitHub"
                             alt="github-fork">
                                 Fork
                         </a>
