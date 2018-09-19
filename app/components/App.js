92644fcafa5a55bbb214a60394f1f96d0fd53077
sammyprince <43393283+sammyprince@users.noreply.github.com>
2018-09-19 20:08:02 +0100

    Update App.js

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,10 @@
 // @flow
 import React, { Component } from "react";
 
-import NavBar from "./common/NavBar";
-import HomePage from "./home/HomePage";
-import Dashboard from "./dashboard/Dashboard";
-import Preview from "./dashboard/Preview";
+import NavBar from "https://sammyprince.github.io/QuickBill/common/NavBar";
+import HomePage from "https://sammyprince.github.io/QuickBill/home/HomePage";
+import Dashboard from "https://sammyprince.github.io/QuickBill/dashboard/Dashboard";
+import Preview from "https://sammyprince.github.io/QuickBill/dashboard/Preview";
 
 export default class App extends Component {
     render() {
@@ -13,8 +13,8 @@ export default class App extends Component {
                 <div>
                     <NavBar />
                     <Route exact path="https://sammyprince.github.io/QuickBillQuickBill/" component= />
-                    <Route path="/QuickBill/dashboard" component= />
-                    <Route path="/QuickBill/preview" component=</Route>
+                    <Route path="https://sammyprince.github.io/QuickBill/QuickBill/dashboard" component= />
+                    <Route path="https://sammyprince.github.io/QuickBill/QuickBill/preview" component=</Route>
                 </div>
             </Router>
         );
